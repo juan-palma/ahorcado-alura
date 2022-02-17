fe92a78cd2961be0da664837aa675b3ce0fe171e
Juan Palma <juan.palma@me.com>
2022-02-17 15:16:06 -0600

    implementacion del service worker

---
+++ sw.js
@@ -0,0 +1,55 @@
+self.addEventListener('install', function (event) {
+	// Instalar de inmediato
+	if (self.skipWaiting) { self.skipWaiting(); }
+	event.waitUntil(
+		caches.open('cache01').then(function (cache) {
+			return cache.addAll([
+				'/',
+				'index.html',
+				'css/main.css',
+				'img/ahorcado_arboles_fondo.png',
+				'img/ahorcado_arboles_frente_fondo.png',
+				'img/ahorcado_escenario_fondo.png',
+				'img/ahorcado_fondo.jpg',
+				'img/ajax.gif',
+				'img/flama.gif',
+				'img/humano.svg',
+				'img/idalibre-logo.svg',
+				'img/juan-palma-logo-obscuro.svg',
+				'img/msnGano.gif',
+				'img/msnPerdio.gif',
+				'img/ovni.svg',
+				'js/librerias/modernizr.js',
+				'js/librerias/parallax.js',
+				'js/librerias/require.js',
+				'js/animaciones/estrellas.js',
+				'js/owner/alertas.js',
+				'js/owner/main.js',
+				'js/owner/validaciones.js'
+			]);
+		})
+	);
+});
+// Cache, falling back to network
+self.addEventListener('fetch', function (event) {
+	event.respondWith(
+		caches.match(event.request).then(function (response) {
+			return response || fetch(event.request);
+		})
+	);
+});
+// Elimina archivos de cache viejos
+var cacheWhitelist = ['cache01'];
+caches.keys().then(function (cacheNames) {
+	return Promise.all(
+		cacheNames.map(function (cacheName) {
+			if (cacheWhitelist.indexOf(cacheName) === -1) {
+				return caches.delete(cacheName);
+			}
+		})
+	);
+});
+caches.keys().then(function (cacheKeys) {
+	// Muestra en la consola la cache instalada 
+	//console.log('Versión SW: ' + cacheKeys);
+});
